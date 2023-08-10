7820f8b67dfd561345b099d06a51e531d561331c
jpaquim <joaopaquim@gmail.com>
2023-08-10 16:44:41 +0100

    Add script to update from upstream repo

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+DIR=../LazyVim
+if [ -d $DIR ]; then
+	pushd $DIR
+	git fetch
+	git reset --hard origin/main
+	git clean -xf
+	popd
+else
+	git clone --depth=1 https://github.com/LazyVim/LazyVim $DIR
+fi
+
+pushd $DIR/lua/lazyvim
+
+mv config/init.lua config.lua
+mv config/*.lua .
+mv util/init.lua util.lua
+
+rm init.lua
+rmdir config util
+
+find . -type f -exec sed -i \
+	-e 's/lazyvim\.config/config/g' \
+	-e 's/lazyvim\.plugins/plugins/g' \
+	-e 's/lazyvim\.util/util/g' \
+	-e 's/config\.init/config/g' \
+	-e 's/config\.options/options/g' \
+	{} +
+
+popd
+
+rsync -a $DIR/lua/lazyvim/ lua/
